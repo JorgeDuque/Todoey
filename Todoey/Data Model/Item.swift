14c0e9c414261b9e0da83d7399a3f069d147fd06
Jorge Duque <jorgeduque71@gmail.com>
2019-06-01 17:11:48 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jorge Duque on 6/1/19.
+//  Copyright © 2019 Jorgeduque. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
